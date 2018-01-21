5e7bc8585f1cc52b920ae40179bf7f230c61639c
miren <miren.cava@gmail.com>
2018-01-21 16:12:06 +0100

    basic c

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
-int main(void)
-{
-  printf("Hello World\n");
-  return 0;
+int main(){
+	int num1 = 10;
+	float pi = 3.141592;
+	char caracter = 'a';
+	char mensaje[] = "Hola";
+ 
+	printf("5 + 5 = %d \n", num1);
+	printf("El número pi es aprox: %f \n", pi);
+	printf("Se puede usar la palabra '%s' para saludar \n", mensaje);
+	printf("La primera letra del abecedario es: %c \n", caracter);
+ 
+	return 0;
 }
